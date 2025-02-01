3e307970f621b070c70dddf34842bde7e798f821
hgun-nugye <huong.ntt.65cntt@ntu.edu.vn>
2025-02-01 21:57:04 +0700

    Add project files.

---
+++ CheckSNT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViDu
+{
+    internal class CheckSNT
+    {
+        public static bool SNT(out int n)
+        {
+            Console.Write("Nhap vao n: ");
+            n=int.Parse(Console.ReadLine());
+            for(int i = 2; i<= n/2; i ++)
+            {
+                if(n%i==0)
+                {
+                    return false;
+                    break;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            if(SNT(out n)) Console.WriteLine("{0} la so nguyen to.",n);
+            else Console.WriteLine("{0} khong phai so nguyen to.",n);
+        }
+    }
+}
---
+++ TongGiaiThua.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViDu
+{
+    internal class TongGiaiThua
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap vao n: ");
+            n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int tg = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                tg *= i;
+                sum += tg;
+                
+            }
+            Console.WriteLine("Tong giai thua la " +sum);
+        }
+    }
+}
